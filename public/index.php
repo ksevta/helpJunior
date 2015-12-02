d44070c2a8e08d507dbd45ac2dc8065102ecf6c1
ksevta <sunnysevta95@gmail.com>
2015-12-03 00:23:34 +0530

    basic page build

--- public/index.php
+++ public/index.php
@@ -2,8 +2,25 @@
 <html>
 <head>
 	<title>helpJunior</title>
+	
+	<link rel="icon" href="images/icon.gif" type="image/gif" sizes="16x16">
+	<link href="stylesheets/style.css" rel="stylesheet">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
 <body>
-	<img type = "image/png" src="1.png">
+<div id = "main">
+	<h1>helpJunior</h1>
+</div>
+<div id = "navbar">
+	<ul>
+			<li><a href="home.php">Home</a></li>
+			<li><a href="about.php">About</a> </li>
+			<li><a href="contact.php">Contact</a> </li>
+	</ul>
+</div>	
+		
 </body>
 </html>
