c25856b1e72024081f8fd87ca4565ad9d14e1ebf
ksevta <sunnysevta95@gmail.com>
2015-12-27 19:05:03 +0530

    pushing public folder (not yet ready)

--- public_temp/includes/footer.php
+++
@@ -1,8 +0,0 @@
-		</div>
-		<div id="footer">Copyright 2015, helpJunior Corporation</div>
-	</body>
-</html>
-<?php
-	// 5. Close connection
-	mysql_close($connection);
-?>
--- public_temp/includes/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-	// This file is the place to store all basic functions
-	
-?>
